6d4ab23da9146843e6cd1724607c5ebf20adb3ba
Rodrigo Luglio <rodrigo@rlabs.com.br>
2025-05-14 18:47:06 -0300

    clean up old files

--- run_clustering_demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to run the clustering demo
-
-echo "Compiling TypeScript..."
-bun build source/services/indexing/emergentIndexing/demos/clusteringDemo.ts --outdir dist
-
-echo "Running clustering demo on current project..."
-node dist/clusteringDemo.js
